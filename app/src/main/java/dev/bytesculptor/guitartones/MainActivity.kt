004cfcb1ff2e3e5897ee1eb09d33fa03a48f99d9
Byte_Sculptor <bytesculptor@gmail.com>
2021-09-20 17:06:00 +0300

    - Change buttons to MaterialDesign, transparent - Add boxes for Setup and Mode on main screen - Fullscreen for MainActivity

--- app/src/main/java/dev/bytesculptor/guitartones/MainActivity.kt
+++ app/src/main/java/dev/bytesculptor/guitartones/MainActivity.kt
@@ -26,6 +26,7 @@ package dev.bytesculptor.guitartones
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.view.WindowManager
 import android.widget.ArrayAdapter
 import android.widget.Spinner
 import androidx.appcompat.app.AppCompatActivity
@@ -45,6 +46,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        this.window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        ) //show the activity in full screen
+
         spTuning = findViewById<View>(R.id.spTuning) as Spinner
         initSpinnerTuning()
         spTone = findViewById<View>(R.id.spToneToFind) as Spinner
